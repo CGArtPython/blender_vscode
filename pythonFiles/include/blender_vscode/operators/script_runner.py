1dea51d14fa189561587d09886a55a97190233b6
CGArtPython <cgartpython@gmail.com>
2023-04-25 22:19:05 -0700

    Support scripts that use if __name__ == "__main__":
    ===
    Changes:
    
    pythonFiles\include\blender_vscode\operators\script_runner.py
    * use run_name kwarg to set the __name__ global variable (see https://docs.python.org/3/library/runpy.html#runpy.run_path )
    * update formatting
    
    ===
    Testing:
    * create script with a main() function and hide the call under if __name__ == "__main__":
    * start Blender from the extention Command Palette
    * run the script from the extention Command Palette

--- pythonFiles/include/blender_vscode/operators/script_runner.py
+++ pythonFiles/include/blender_vscode/operators/script_runner.py
@@ -3,13 +3,13 @@ import re
 import bpy
 import runpy
 from bpy.props import *
-from .. utils import redraw_all
-from .. communication import register_post_action
+from ..utils import redraw_all
+from ..communication import register_post_action
 
 
 def run_script(path):
     context = prepare_script_context(path)
-    runpy.run_path(path, init_globals={"CTX": context})
+    runpy.run_path(path, run_name="__main__", init_globals={"CTX": context})
     redraw_all()
 
 
@@ -23,8 +23,8 @@ def prepare_script_context(filepath):
     with open(filepath) as fs:
         text = fs.read()
 
-    area_type = 'VIEW_3D'
-    region_type = 'WINDOW'
+    area_type = "VIEW_3D"
+    region_type = "WINDOW"
 
     for line in text.splitlines():
         match = re.match(r"^\s*#\s*context\.area\s*:\s*(\w+)", line, re.IGNORECASE)
@@ -42,17 +42,20 @@ def prepare_script_context(filepath):
     context["region"] = get_region_in_area(context["area"], region_type) if context["area"] else None
     return context
 
+
 def get_area_by_type(area_type):
     for area in bpy.data.window_managers[0].windows[0].screen.areas:
         if area.type == area_type:
             return area
     return None
 
+
 def get_region_in_area(area, region_type):
     for region in area.regions:
         if region.type == region_type:
             return region
     return None
+
 
 def register():
     register_post_action("script", run_script_action)
